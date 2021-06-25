d39558b8c36b98ab2b3b15697c1c47a0660193ce
olivier <orefalo@gmail.com>
2021-06-25 13:21:32 +0400

    ch

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+helm install --create-namespace -n mu mu .
--- dryrun.sh
+++ dryrun.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 set -ex
-helm install --dry-run --debug mu mu
+helm install --dry-run --debug --create-namespace mu -n mu mu
